c4095e335a96790ff522e1ec789ab7ad7c59e696
Shubham Singh <shuhbam.singh@tothenew.com>
2022-06-14 16:54:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+// const app = require("express")();
+// const http = require("http").createServer(app);
+// const io = require("socket.io")(http);
+// const cors = require("cors");
+// const PORT = process.env.PORT || 5000;
+// const { addUser, getUser, deleteUser, getUsers } = require("./users");
+
+// app.use(cors());
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected :D");
+//   socket.on("chat message", (msg) => {
+//     console.log(msg);
+//     io.emit("chat message", msg);
+//   });
+
+//   socket.on("login", ({ name, room }, callback) => {
+//     const { user, error } = addUser(socket.id, name, room);
+//     if (error) return callback(error);
+//     socket.join(user.room);
+//     socket
+//       .in(room)
+//       .emit("notification", {
+//         title: "Someone's here",
+//         description: `${user.name} just entered the room`,
+//       });
+//     io.in(room).emit("users", getUsers(room));
+//     callback();
+//   });
+
+//   socket.on("sendMessage", (message) => {
+//     const user = getUser(socket.id);
+//     io.in(user.room).emit("message", { user: user.name, text: message });
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("User disconnected");
+//     const user = deleteUser(socket.id);
+//     if (user) {
+//       io.in(user.room).emit("notification", {
+//         title: "Someone just left",
+//         description: `${user.name} just left the room`,
+//       });
+//       io.in(user.room).emit("users", getUsers(user.room));
+//     }
+//   });
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Server is up and running");
+// });
+
+// http.listen(PORT, () => {
+//   console.log(`Listening to ${PORT}`);
+// });
+
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = 3000;
+
+io.on("connection", (socket) => {
+  console.log("a user connected :D");
+  socket.on("chat message", (msg) => {
+    console.log(msg);
+    io.emit("chat message", msg);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
+});
+
+server.listen(port, () => console.log("server running on port:" + port));
